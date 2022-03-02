bcb5ec815db3fa7b435fb09eccef033c7484cb15
Saba Jahedi <sabajahedi.sj@gmail.com>
2022-03-02 15:02:28 +0330

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by SabaJahedi on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
